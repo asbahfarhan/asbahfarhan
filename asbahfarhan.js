f9980c573811fed3389ecd3de7f17b04f53ca80a
asbahfarhan <asbahfarhan1@icloud.com>
2024-05-11 01:51:25 +0300

    Update asbahfarhan.js

--- asbahfarhan.js
+++ asbahfarhan.js
@@ -11,11 +11,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 // Add a marker to the map for New York City
-var luigismarker = L.marker([40.68965691085725, -73.96537769031497]).addTo(map);
+var luigismarker = L.marker([40.57123511102176, -74.14576783441058]).addTo(map);
 
 // Add a popup to the marker
 luigismarker.bindPopup(`
-    <b>Luigi's Pizza</b><br>Best pizza on Dekalb Ave<br>
+    <b>eggers ice cream parlor</b><br>eggers ice cream parlor<br>
     <img src="https://aurashktest.github.io/aurashktest/images/luigis.png" style="width: 100px; height: auto;">
 `).openPopup();
 
