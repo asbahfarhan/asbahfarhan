15ae570386c6b7dda3be9eff381af82a626b6012
asbahfarhan <asbahfarhan1@icloud.com>
2024-05-11 03:04:00 +0300

    Update asbah.js

--- asbah.js
+++ asbah.js
@@ -1,17 +1,20 @@
 // Initialize Leaflet map
-var map = L.map("nymap").setView([40.681801312931455, -73.9571896025164],13); // Set center to New York City and zoom level
+var map = L.map('nymap').setView([40.58350424607736, -74.15069944985474], 13); // Set center to New York City and zoom level
 
 // Add a tile layer to the map (for example, OpenStreetMap)
-/*L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
+
+
+
+
 // Add a marker to the map for New York City
-var luigismarker = L.marker([40.571186213395016, -74.14575710557477]).addTo(map);
+var luigismarker = L.marker([40.57407929007067, -74.14246335141281]).addTo(map);
 
 // Add a popup to the marker
 luigismarker.bindPopup(`
-    <b>Eggers Ice Cream Parlor</b><br>
-    Eggers Ice Cream Parlor<br>
+    <b>EggsIceCream Parlor/br>
     <img src="https://asbahfarhan.github.io/asbahfarhan/EggersIceCreamPalor.png" style="width: 100px; height: auto;">
-`).openPopup();*/
+`).openPopup();
