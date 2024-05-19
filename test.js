89c422ef85c5ca6b06d0841f0ee45630cbcd5658
asbahfarhan <asbahfarhan1@icloud.com>
2024-05-19 06:36:59 +0300

    Update test.js

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //var luigismarker = L.marker([40.571210662212856, -74.14542451166442]).addTo(map);
 
 // Add a popup to the marker
-//luigismarker.bindPopup(`
+luigismarker.bindPopup(`
    // <b>Eggers IceCream Parlor</b>
    // <img src="https://asbahfarhan.github.io/asbahfarhan/EggersIceCreamParlor.png" style="width: 250px; height: 250px">
 //`).openPopup();
